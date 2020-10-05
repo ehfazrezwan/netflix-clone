e44bff85c3089a7d6dd29a0f9416352fee7aa169
unknown <ehfaz.rezwan@gmail.com>
2020-10-06 02:38:52 +0600

    Navbar + Styling added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       {/* Navbar */}
-      <Nav></Nav>
+      <Nav />
       {/* Banner */}
       <Banner />
       <Row
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 
+import "./Nav.css";
+
 function Nav() {
-  const [show, handleShow] = useState([]);
+  const [show, handleShow] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
@@ -15,9 +17,19 @@ function Nav() {
   }, []);
 
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       {/* Netflix logo */}
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+        alt="Netflix logo"
+      />
       {/* Avatar logo  */}
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="Netflix logo"
+      />
     </div>
   );
 }
